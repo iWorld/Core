d517351d33cc9bd3ae418a5411f3b13d0542790a
Siguza <molt@gmx.net>
2012-04-19 16:05:04 +0200

    Blubb
    Bli Bla Blubb

---
+++ src/net/drgnome/iworld/iPlugin.java
@@ -0,0 +1,33 @@
+// Bukkit Plugin "World" by Siguza and steffengy
+// This software is distributed under the following license:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+package net.drgnome.iworld;
+
+import java.util.logging.Logger;
+
+import net.minecraft.server.*;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class iPlugin extends JavaPlugin
+{
+    public static String version = "Alpha 0.1.0";
+    private Logger log = Logger.getLogger("Minecraft");
+
+    public void onEnable()
+    {
+        log.info("Enabling iWorld " + version);
+    }
+
+    public void onDisable()
+    {
+        log.info("Disabling iWorld " + version);
+    }
+    
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id)
+    {
+        return new iWorld(id);
+    }
+}
